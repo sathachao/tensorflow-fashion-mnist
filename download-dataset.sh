e6c263d649068a51fedeba648151c8201f5f3553
Satha Chaojaroenrat <sathachao@gmail.com>
2019-01-10 10:37:45 +0700

    Update dataset downloading process

--- download-dataset.sh
+++ download-dataset.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -e
+
 mkdir -p data/fashion
 
 echo "Downloading Fashion MNIST data"
@@ -8,3 +12,14 @@ wget https://github.com/zalandoresearch/fashion-mnist/raw/master/data/fashion/tr
 wget https://github.com/zalandoresearch/fashion-mnist/raw/master/data/fashion/train-labels-idx1-ubyte.gz -O data/fashion/train-labels-idx1-ubyte.gz
 
 echo "Finished downloading"
+
+echo "Validating checksums"
+echo '8d4fb7e6c68d591d4c3dfef9ec88bf0d data/fashion/train-images-idx3-ubyte.gz' | md5sum -c
+echo '25c81989df183df01b3e8a0aad5dffbe data/fashion/train-labels-idx1-ubyte.gz' | md5sum -c
+echo 'bef4ecab320f06d8554ea6380940ec79 data/fashion/t10k-images-idx3-ubyte.gz' | md5sum -c
+echo 'bb300cfdad3c16e7a12a480ee83cd310 data/fashion/t10k-labels-idx1-ubyte.gz' | md5sum -c
+echo "Validation completed: No errors"
+
+echo "Unzipping data"
+gzip -dv data/fashion/*.gz
+echo "Finished unzipping"
